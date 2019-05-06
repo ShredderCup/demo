44a86039325534af25d966d1f3975505d9c83fd3
Shredder Cup <https://github.com/ShredderCup/test1.git>
2019-05-06 10:54:17 +0800

    第一次提交

---
+++ src/main/java/com/czq/pojo/DishTable.java
@@ -0,0 +1,45 @@
+package com.czq.pojo;
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class DishTable {
+
+    private Integer id;
+    private  String name;
+    private Integer status;
+    private Date orderTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(Date orderTime) {
+        this.orderTime = orderTime;
+    }
+}
